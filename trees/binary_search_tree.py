aebe97c77133085d6964a3e2d8a8416fae1cbcd4
Sujit-Anireddy <anireddysujitreddy@gmail.com>
2026-05-06 14:56:16 -0500

    inserting a node to BST completed

--- trees/binary_search_tree.py
+++ trees/binary_search_tree.py
@@ -2,19 +2,28 @@
 
 from binary_tree import TreeNode
 
-def search_bst(node, target):
-    
+#TC: O(logn)
+def search_bst(node, target):    
     if not node:
         return False
 
     if node.val == target:
         return True
-
     if node.val > target: 
         return search_bst(node.left, target)
     else:
         return search_bst(node.right, target)
 
+#TC: O(logn)
+def insert(root ,val):
+    if not root:
+        return TreeNode(val)
+
+    if root.val < val:
+        root.right = insert(root.right, val)
+    elif root.val > val:
+        root.left = insert(root.left, val)
+    return root
 
 
 #testing 
@@ -39,4 +48,5 @@ C2.left, C2.right = F2, G2
 
 #print(A2)
 #TreeNode.recursive_inorder(A2)
-print(search_bst(A2, 100))
+#print(search_bst(A2, 100))
+print(insert(A2, 14))
